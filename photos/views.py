9b29553d887144be836bc48a0e80ff3c940033a8
MAKONJIO99 <francismakonjio2@gmail.com>
2021-07-05 15:27:32 +0300

    create render location views

---
+++ photos/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Image,Location
+# Create your views here.
+def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'photos/index.html', {'images': images[::-1], 'locations': locations})
