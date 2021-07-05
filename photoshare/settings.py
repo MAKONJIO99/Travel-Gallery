f04ba84233452cb5c67f6521fea36fe31e4e62ea
MAKONJIO99 <francismakonjio2@gmail.com>
2021-07-05 20:39:02 +0300

    deployment

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -120,8 +120,13 @@ USE_TZ = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
